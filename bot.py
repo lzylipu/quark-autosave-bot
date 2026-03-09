e16f9b195537b320c451426485796b91662fead0
lllbot <91266101+lzylipu@users.noreply.github.com>
2026-03-09 23:15:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
-import nonebot
+import sys
 
-# Import Adapters
-from nonebot.adapters.telegram import Adapter as TelegramAdapter  # type: ignore
+import nonebot
+from nonebot.adapters.telegram import Adapter as TelegramAdapter
+
+# 强制优先从源码目录加载插件
+sys.path.insert(0, "/app/src")
 
 nonebot.init()
-driver = nonebot.get_driver()
 
-# Register Adapters
+driver = nonebot.get_driver()
 driver.register_adapter(TelegramAdapter)
 
-nonebot.load_plugin("nonebot_plugin_quark_autosave")
+# 从源码目录加载插件，而不是加载 site-packages 里的已安装版本
+nonebot.load_plugins("/app/src")
 
 if __name__ == "__main__":
     nonebot.run()
